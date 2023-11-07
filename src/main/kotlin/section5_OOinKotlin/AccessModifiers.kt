735be3123ccc85a49fd5dcda1160ac85cad8e39d
marwhals <marjan.mubarok2012@gmail.com>
2023-11-07 22:53:56 +0000

    Declaring Classes and Using Constructors in Kotlin

--- src/main/kotlin/section5_OOinKotlin/AccessModifiers.kt
+++ src/main/kotlin/section5_OOinKotlin/AccessModifiers.kt
@@ -5,11 +5,46 @@ fun main(args: Array<String>) {
     - In Kotlin the default visbility is public
     - internal - visibile in the same modules
      */
-    val emp = Employee()
+    val emp = Employee("Bob")
     println(emp)
+
+    val emp2 = Employee("Joe")
+    println(emp.firstName)
+    println(emp.fullTime)
+
+    val emp3 = Employee("Drew")
+    println(emp.firstName)
+    println(emp.fullTime)
+
+    println(Demo().dummy)
 
 }
 
-private class Employee {
-        // This is visible to everything in the same file
+/**
+ * When A constructor is declared outside of the curly braces it is known as a primary constructor
+ */
+//private class protected constructor(val firstName: String)
+//private class Employee(val firstName: String){
+//    var fullTime: Boolean = true
+//
+//    constructor(firstName: String, fullTime: Boolean): this(firstName) {
+//        this.fullTime = fullTime
+//    }
+//
+////    val firstName: String = firstName
+//
+////    init {
+//        // This block is used in conjunction with the primary constructor
+////        this.firstName = firstName
+////    }
+//}
+
+class Employee(val firstName: String, var fullTime: Boolean = true)
+
+class Demo {
+    val dummy: String
+
+    constructor() {
+        dummy = "Hello"
+    }
 }
