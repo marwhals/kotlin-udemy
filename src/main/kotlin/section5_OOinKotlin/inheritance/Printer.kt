6cc67b7dfede7615f5ef835a647adbd2d73d2dd7
marwhals <marjan.mubarok2012@gmail.com>
2023-11-09 00:10:42 +0000

    44 - Interfaces

--- src/main/kotlin/section5_OOinKotlin/inheritance/Printer.kt
+++ src/main/kotlin/section5_OOinKotlin/inheritance/Printer.kt
@@ -1,7 +1,7 @@
 package section5_OOinKotlin.inheritance
 
 fun main(args: Array<String>) {
-    val laserPrinter = LaserPrinter("Brother 1234", 15)
+    val laserPrinter = LaserPrinter("Brother 1234", 15, 23)
     laserPrinter.printModel()
 
     SomethingElse("Whatever")
@@ -15,29 +15,56 @@ abstract class Printer(val modelName: String) {
     abstract fun bestSellingPrice(): Double
 }
 
-open class LaserPrinter(modelName: String, ppm: Int) : Printer(modelName) {
+open class LaserPrinter(modelName: String, ppm: Int, override val number: Int) : Printer(modelName), MyInterface {
 
     //override functions are also open
     final override fun printModel() = println("The model name of this laser printer is $modelName")
     override fun bestSellingPrice(): Double = 1291.99
+    //This is from the interface
+    override fun myFunction(str: String): String {
+        TODO("Not yet implemented")
+    }
 }
 
-class SpecialLaserPrinter(modelName: String) : LaserPrinter(modelName, 12) {
+class SpecialLaserPrinter(modelName: String) : LaserPrinter(modelName, 12, 23) {
 
 }
 
-open class Something {
+open class Something: MySubInterface{
 
     val someProperty: String
+    override val number: Int = 25
 
     constructor(someParameter: String) {
         someProperty = someParameter
         println("I'm in the parents constructor")
     }
+
+    override fun mySubFunction(num: Int): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun myFunction(str: String): String {
+        TODO("Not yet implemented")
+    }
+
+
 }
 
 class SomethingElse : Something {
     constructor(someOtherParameter: String) : super(someOtherParameter) {
         println("I'm in the child's constructor")
     }
+}
+
+interface MyInterface {
+    val number: Int
+    val number2: Int
+        get() = number * 100
+
+    fun myFunction(str: String): String
+}
+
+interface MySubInterface: MyInterface {
+    fun mySubFunction(num: Int): String
 }
