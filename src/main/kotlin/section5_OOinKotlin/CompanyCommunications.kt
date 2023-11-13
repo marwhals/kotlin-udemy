107fff0685b0ae4d8865d8893e9d9b9e46cc5a1d
marwhals <marjan.mubarok2012@gmail.com>
2023-11-13 00:08:11 +0000

    46 - Companion objects

--- src/main/kotlin/section5_OOinKotlin/CompanyCommunications.kt
+++ src/main/kotlin/section5_OOinKotlin/CompanyCommunications.kt
@@ -9,6 +9,14 @@ import java.time.Year
 fun main(args: Array<String>) {
     println(CompanyCommunications.getTagLine())
     println(CompanyCommunications.getCopyrightLine())
+
+    println(SomeClass.Companion.accessPrivateCar()) //Don't actually have to use the word companion
+
+    val someClass1 = SomeClass.justAssign("This is the string as is")
+    val someClass2 = SomeClass.upperOrLowerCase("this isn't the string as is", false)
+    println(someClass1.someString)
+    println(someClass2.someString)
+//    val someClass3 = SomeClass() <-- this line won't compile it needs to go through the companion object
 }
 
 object CompanyCommunications {
@@ -19,4 +27,45 @@ object CompanyCommunications {
     fun getCopyrightLine() = "Copyright \u00A9 $currentYear Our Company. All right reserved"
 
 
+}
+/*
+- Implementing the factory pattern
+- i.e the companion object is responsisble for creating instances
+- the primary constructor has been marked as private
+ */
+class SomeClass private constructor(val someString: String) {
+    /*
+    - companion object SomeCompanion <- could use a name
+     */
+//    val someString: String
+//
+//    /*
+//    - Declaring two secondary constructors
+//     */
+//
+//    constructor(str: String) {
+//        someString = str
+//    }
+//
+//    constructor(str: String, lowerCase: Boolean) {
+//        if (lowerCase) {
+//            someString = str.toUpperCase()
+//        } else
+//            someString = str.toLowerCase()
+//    }
+
+    companion object {
+        private var privateVar = 6
+
+        fun accessPrivateCar() = "I'm accessing privateVar: $privateVar"
+
+        fun justAssign(str: String) = SomeClass(str)
+        fun upperOrLowerCase(str: String, lowerCase: Boolean): SomeClass {
+            if (lowerCase) {
+                return SomeClass(str.toLowerCase())
+            } else {
+                return SomeClass(str.toUpperCase())
+            }
+        }
+    }
 }
