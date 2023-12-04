c66042ca7369f36ad5b933dfeb285dea01e3e256
marwhals <marjan.mubarok2012@gmail.com>
2023-12-04 01:06:09 +0000

    73 - Use-site variance

--- src/main/kotlin/section7LambdasCollectionsGenerics/Contravariance/UseSiteVariance.kt
+++ src/main/kotlin/section7LambdasCollectionsGenerics/Contravariance/UseSiteVariance.kt
@@ -1,4 +1,34 @@
 package section7LambdasCollectionsGenerics.Contravariance
 
-class UseSiteVariance {
+fun main(args: Array<String>) {
+    val cars1 = mutableListOf(Car(), Car())
+    val cars2: MutableList<Car> = mutableListOf()
+    copyCars(cars1, cars2)
+
+    val fords1 = mutableListOf(Ford(), Ford())
+    val fords2: MutableList<Ford> = mutableListOf()
+    copyCars(fords1, fords2)
+
+    copyCars(fords1, cars2)
+
+    val cars3: MutableList<Car> = mutableListOf(Ford(), Ford())
+
+}
+// Example of using use site covariance. Covariance where the function is using a certain type
+fun <T: Car> copyCars(source: MutableList<out T>, destination: MutableList<T>) {
+    for (car in source) {
+        destination.add(car)
+    }
+}
+
+open class Car {
+
+}
+
+class Toyota: Car() {
+
+}
+
+class Ford: Car() {
+
 }
