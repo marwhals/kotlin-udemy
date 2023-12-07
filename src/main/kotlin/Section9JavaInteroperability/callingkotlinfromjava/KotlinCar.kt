20033c9e72ed27c31658f0a459a5f26b116c06c7
marwhals <marjan.mubarok2012@gmail.com>
2023-12-07 15:12:20 +0000

    83 - Annotations when calling Kotlin from Java

--- src/main/kotlin/Section9JavaInteroperability/callingkotlinfromjava/KotlinCar.kt
+++ src/main/kotlin/Section9JavaInteroperability/callingkotlinfromjava/KotlinCar.kt
@@ -2,13 +2,46 @@
 
 package Section9JavaInteroperability.callingkotlinfromjava
 
+import java.io.IOException
+
+/*
+- The @static annotation tell the compiler to generate two versions of the function
+- One within the instance and also a static one that can be called form Java
+ */
+
 fun topLevel() = println("I'm in the Car file!")
 
 fun main(args: Array<String>) {
     "Print this".print()
+    KotlinCar.carComp() //Notice the difference with what is in Main
 }
+
+@JvmOverloads fun defaultArgs(str: String, num: Int = 25) {
+
+}
+
+@Throws(IOException::class)
+fun doIO() {
+    throw IOException()
+}
+
+object SingletonObj {
+    @JvmStatic fun doSomething() = println("I'm doing something in the singleton object")
+}
+
 // See documentation @JvmField <- dont generate getters and setters
 class KotlinCar(val color: String, @JvmField val model: String, val year: Int, val isAutomatic: Boolean) {
+
+
+    companion object { //Jvm static.... compiler will generate both versions of the code. A static one for Java
+        const val constant = 25; // Doesn't need any kind of annotation modifier
+        @JvmField val isAuto = false
+        @JvmStatic fun carComp() = println("I'm in Car's companion object")
+    }
+
+    fun printMe(text: String) {
+        println("I don't expect a null value: $text")
+    }
 
 //    var color: String = color
 //        private set(value) {
--- src/main/kotlin/Section9JavaInteroperability/callingkotlinfromjava/Main.java
+++ src/main/kotlin/Section9JavaInteroperability/callingkotlinfromjava/Main.java
@@ -3,16 +3,42 @@ package Section9JavaInteroperability.callingkotlinfromjava;
 import Section9JavaInteroperability.callingkotlinfromjava.KotlinCar; //These imports are for demonstration
 import Section9JavaInteroperability.callingkotlinfromjava.StaticCar; //See the KotlinCar class file
 
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-	    StaticCar.topLevel();
-	    StaticCar.print("print this Java string");
+        StaticCar.topLevel();
+        StaticCar.print("print this Java string");
 
-	    KotlinCar car = new KotlinCar("blue", "BMW", 2011, true);
-	    System.out.println(car.model);
-	    System.out.println(car.getColor());
+        KotlinCar car = new KotlinCar("blue", "BMW", 2011, true);
+        System.out.println(car.model);
+        System.out.println(car.getColor());
         System.out.println(car.isAutomatic());
+
+        KotlinCar.carComp();// The static kotlin method call
+        SingletonObj.doSomething();
+
+//        System.out.println("isAuto = " + KotlinCar.isAuto());
+        System.out.println("Constant from kotlin class" + KotlinCar.constant);
+
+//        car.printMe(null);// This will throw an illegal argument exception
+//        StaticCar.doIO();// This will throw an IO exception
+
+
+        //The below won't compile even the kotlin code throws the exception ---> Solution is @Throws(IOException::class)
+        try {
+            StaticCar.doIO();// This will throw an IO exception
+        } catch (IOException e) {
+            System.out.println("IOException!!!!! from KotlinCar");
+        }
+
+        /*
+        - This only works in Java when both parameters are supplied
+        - Only one version of the kotlin function exists before using the overloads annotation
+         */
+        StaticCar.defaultArgs("The number is: ", 40);
+        StaticCar.defaultArgs("The number is: "); //This works because of the annotation in the KotlinCar class
 
     }
 }
